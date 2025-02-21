b52c8286caca080bbec0caade509634f5e123a1e
afharyo <afharyodiwseno@gmail.com>
2025-02-21 19:44:50 +0700

    update home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,6 @@
+import 'package:first_project/pages/tambah_kurang.dart';
+import 'package:first_project/pages/ganjil_genap.dart';
 import 'package:first_project/pages/count_number.dart';
-import 'package:first_project/pages/login.dart';
-import 'package:first_project/pages/home.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,34 +8,36 @@ class HomePage extends StatelessWidget {
 
   final String text;
 
-  //LoginPage({Key key, required this.text}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
       body: Column(
         children: [
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              alignment: Alignment.center,
-              child: Text(
-                "Feature Menu",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                alignment: Alignment.center,
+                child: Text(
+                  "Feature Menu",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
               ),
-            )
-          ]),
+            ],
+          ),
           Padding(
             padding: EdgeInsets.only(top: 10),
             child: menu(context),
           ),
           Container(
-              margin: EdgeInsets.only(top: 50),
-              child: Text(
-                "Developed By",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              )),
+            margin: EdgeInsets.only(top: 50),
+            child: Text(
+              "Developed By",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+          ),
           Padding(
             padding: EdgeInsets.only(top: 10),
             child: author(),
@@ -55,260 +57,211 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Row menu(BuildContext context){
+  Row menu(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TambahKurang()),
+            );
+            print("Container dan teks diklik!");
+          },
+          child: Column(
             children: [
-              GestureDetector(
-                onTap: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => TambahKurang()),
-                  );
-                  print("Container dan teks diklik!");
-                },
-                child: Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.white, // Warna latar belakang
-                        borderRadius: BorderRadius.circular(12), // Membuat sudut melengkung
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black26, // Warna shadow dengan transparansi
-                            blurRadius: 8, // Membuat shadow lebih halus
-                            spreadRadius: 2, // Menyebarkan shadow
-                            offset: Offset(4, 4), // Posisi shadow (X, Y)
-                          ),
-                        ],
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(12), // Pastikan gambar ikut melengkung
-                        child: Image.asset("assets/icons/oddeven.png", fit: BoxFit.scaleDown),
-                      ),
+              Container(
+                margin: EdgeInsets.all(10),
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 8,
+                      spreadRadius: 2,
+                      offset: Offset(4, 4),
                     ),
-                    Text('Tambah Kurang',)
                   ],
                 ),
-              ),
-              
-              
-              GestureDetector(
-                onTap: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => GanjilGenap()),
-                  );
-                  print("Container dan teks diklik!");
-                },
-                child: Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.white, // Warna latar belakang
-                        borderRadius: BorderRadius.circular(12), // Membuat sudut melengkung
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black26, // Warna shadow dengan transparansi
-                            blurRadius: 8, // Membuat shadow lebih halus
-                            spreadRadius: 2, // Menyebarkan shadow
-                            offset: Offset(4, 4), // Posisi shadow (X, Y)
-                          ),
-                        ],
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(12), // Pastikan gambar ikut melengkung
-                        child: Image.asset("assets/icons/oddeven.png", fit: BoxFit.scaleDown),
-                      ),
-                    ),
-                    Text('Ganjil Genap')
-                  ],
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset("assets/icons/oddeven.png",
+                      fit: BoxFit.scaleDown),
                 ),
               ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(
-                    12), // Pastikan gambar ikut melengkung
-                child: Image.asset("assets/icons/oddeven.png",
-                    fit: BoxFit.scaleDown),
-              ),
-            ),
-            Text('Ganjil Genap')
-          ],
+              Text('Tambah Kurang')
+            ],
+          ),
         ),
-      ),
-      GestureDetector(
-        onTap: () {
-          print("Container dan teks diklik!");
-        },
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10),
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                color: Colors.white, // Warna latar belakang
-                borderRadius:
-                    BorderRadius.circular(12), // Membuat sudut melengkung
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26, // Warna shadow dengan transparansi
-                    blurRadius: 8, // Membuat shadow lebih halus
-                    spreadRadius: 2, // Menyebarkan shadow
-                    offset: Offset(4, 4), // Posisi shadow (X, Y)
-                  ),
-                ],
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => GanjilGenap()),
+            );
+            print("Container dan teks diklik!");
+          },
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 8,
+                      spreadRadius: 2,
+                      offset: Offset(4, 4),
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset("assets/icons/oddeven.png",
+                      fit: BoxFit.scaleDown),
+                ),
               ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(
-                    12), // Pastikan gambar ikut melengkung
-                child: Image.asset("assets/icons/oddeven.png",
-                    fit: BoxFit.scaleDown),
-              ),
-            ),
-            Text('Ganjil Genap')
-          ],
+              Text('Ganjil Genap')
+            ],
+          ),
         ),
-      ),
-      GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => CountNumber()),
-          );
-          print("Container dan teks diklik!");
-        },
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10),
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                color: Colors.white, // Warna latar belakang
-                borderRadius:
-                    BorderRadius.circular(12), // Membuat sudut melengkung
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26, // Warna shadow dengan transparansi
-                    blurRadius: 8, // Membuat shadow lebih halus
-                    spreadRadius: 2, // Menyebarkan shadow
-                    offset: Offset(4, 4), // Posisi shadow (X, Y)
-                  ),
-                ],
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CountNumber()),
+            );
+            print("Container dan teks diklik!");
+          },
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 8,
+                      spreadRadius: 2,
+                      offset: Offset(4, 4),
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset("assets/icons/oddeven.png",
+                      fit: BoxFit.scaleDown),
+                ),
               ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(
-                    12), // Pastikan gambar ikut melengkung
-                child: Image.asset("assets/icons/oddeven.png",
-                    fit: BoxFit.scaleDown),
-              ),
-            ),
-            Text('Count Number')
-          ],
+              Text('Count Number')
+            ],
+          ),
         ),
-      ),
-    ]);
+      ],
+    );
   }
 
   Row author() {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            height: 100,
-            width: 100,
-            decoration: BoxDecoration(
-              color: Colors.white, // Warna latar belakang
-              borderRadius:
-                  BorderRadius.circular(12), // Membuat sudut melengkung
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26, // Warna shadow dengan transparansi
-                  blurRadius: 8, // Membuat shadow lebih halus
-                  spreadRadius: 2, // Menyebarkan shadow
-                  offset: Offset(4, 4), // Posisi shadow (X, Y)
-                ),
-              ],
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 8,
+                    spreadRadius: 2,
+                    offset: Offset(4, 4),
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset("assets/icons/human1.jpg",
+                    fit: BoxFit.scaleDown),
+              ),
             ),
-            child: ClipRRect(
-              borderRadius:
-                  BorderRadius.circular(12), // Pastikan gambar ikut melengkung
-              child:
-                  Image.asset("assets/icons/human1.jpg", fit: BoxFit.scaleDown),
+            Text('Veri Anggoro W'),
+            Text('123220000')
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 8,
+                    spreadRadius: 2,
+                    offset: Offset(4, 4),
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset("assets/icons/human2.jpg",
+                    fit: BoxFit.scaleDown),
+              ),
             ),
-          ),
-          Text('Veri Anggoro W'),
-          Text('123220000')
-        ],
-      ),
-      Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            height: 100,
-            width: 100,
-            decoration: BoxDecoration(
-              color: Colors.white, // Warna latar belakang
-              borderRadius:
-                  BorderRadius.circular(12), // Membuat sudut melengkung
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26, // Warna shadow dengan transparansi
-                  blurRadius: 8, // Membuat shadow lebih halus
-                  spreadRadius: 2, // Menyebarkan shadow
-                  offset: Offset(4, 4), // Posisi shadow (X, Y)
-                ),
-              ],
+            Text('Azhar Fikri A'),
+            Text('123220000')
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 8,
+                    spreadRadius: 2,
+                    offset: Offset(4, 4),
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset("assets/icons/human3.jpg",
+                    fit: BoxFit.scaleDown),
+              ),
             ),
-            child: ClipRRect(
-              borderRadius:
-                  BorderRadius.circular(12), // Pastikan gambar ikut melengkung
-              child:
-                  Image.asset("assets/icons/human2.jpg", fit: BoxFit.scaleDown),
-            ),
-          ),
-          Text('Azhar Fikri A'),
-          Text('123220000')
-        ],
-      ),
-      Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            height: 100,
-            width: 100,
-            decoration: BoxDecoration(
-              color: Colors.white, // Warna latar belakang
-              borderRadius:
-                  BorderRadius.circular(12), // Membuat sudut melengkung
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26, // Warna shadow dengan transparansi
-                  blurRadius: 8, // Membuat shadow lebih halus
-                  spreadRadius: 2, // Menyebarkan shadow
-                  offset: Offset(4, 4), // Posisi shadow (X, Y)
-                ),
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius:
-                  BorderRadius.circular(12), // Pastikan gambar ikut melengkung
-              child:
-                  Image.asset("assets/icons/human3.jpg", fit: BoxFit.scaleDown),
-            ),
-          ),
-          Text('Re Faiza Depta P'),
-          Text('123220050')
-        ],
-      ),
-    ]);
+            Text('Re Faiza Depta P'),
+            Text('123220050')
+          ],
+        ),
+      ],
+    );
   }
 }
